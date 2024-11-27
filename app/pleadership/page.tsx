adf331290b68a4654cd238adce8c53f7f7ab8012
Colin Hermack <chermack@purdue.edu>
2024-11-26 22:42:04 -0500

    Added some rudimentary code for the pleadership page - needs work

--- app/pleadership/page.tsx
+++ app/pleadership/page.tsx
@@ -1,41 +1,43 @@
 import { title } from "@/components/primitives";
-const { Client } = require('pg');  //PostgreSQL
-
-async function getLeaderData() {
-    let client = new Client({
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_DATABASE,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
-
-    try {
-        await client.connect();
-        const result = await client.query(
-            `SELECT officer.position, 
-                officer.officer_data,
-                member.name, 
-                member.email, 
-                member.pronouns, 
-                member.phone
-            FROM officer
-            JOIN member ON officer.member_id = member.member_id`);
-        return result.rows
-    } catch(error: any) {
-        console.error(error);
-    }
-}
+import { getLeaderData } from '@/utils/leadership';
+import Image from 'next/image';
 
 export default async function AboutPage() {
     const data = await getLeaderData();
-    console.log(data);
+
     return (
         <div>
             <h1 className={title()}>Pleadership</h1>
+            <div className='flex flex-col justify-top items-center text-xl'>
+                {data.map((item) => {
+                    return(<div>
+                        <h2 className='mt-4'>{item.label}</h2>
+                        <div className='flex flex-wrap'>
+                            {item.content.map(currOfficer => {
+                                return (
+                                    <div className='py-4'>
+                                        <div className='pb-0 pt-2 px-4 flex-col items-start'>
+                                            <p className="text-tiny uppercase font-bold">{currOfficer.name}</p>
+                                            <small className="text-default-500">{currOfficer.pronouns}</small>
+                                            <h4 className="font-bold text-large">{currOfficer.position}</h4>
+                                        </div>
+                                        <div className="overflow-visible py-2">
+                                        <Image
+                                            alt={currOfficer.name}
+                                            className="rounded-xl"
+                                            src={`/leadership/${currOfficer.officer_data.ImagePath}`}
+                                            width={200}
+                                            height={200}
+                                        />
+                                        </div>
+
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>)
+                })}
+            </div>
         </div>
     );
 }

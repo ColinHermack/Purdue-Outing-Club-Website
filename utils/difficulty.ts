25e795f7ecde97d9c3b9d533e89b9247cdad7504
Colin Hermack <chermack@purdue.edu>
2025-02-05 17:14:50 -0500

    Added helper functions to get trip difficulty descriptions

---
+++ utils/difficulty.ts
@@ -0,0 +1,13 @@
+import { tripDifficultyDescriptions } from "@/config/tripDifficultyDescriptions";
+
+export function getTripDifficultyDescription(difficulty: number, sport: string): string {
+    if (!Object.keys(tripDifficultyDescriptions).includes(sport)) {
+        return "";
+    }
+
+    if (tripDifficultyDescriptions[sport].length < difficulty) {
+        return tripDifficultyDescriptions[sport][tripDifficultyDescriptions[sport].length - 1];
+    }
+
+    return tripDifficultyDescriptions[sport][difficulty - 1];
+}

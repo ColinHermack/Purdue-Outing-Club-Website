da0bccd3e3a95f1bb2f663e6873e4284f47a1889
Eli Orlov <elisey.orlof@gmail.com>
2026-06-02 11:33:06 -0400

    consolidates trips.ts and tripsMiniService.ts

--- miniservices/tripMiniService.ts
+++ miniservices/tripMiniService.ts
@@ -44,3 +44,41 @@ export const getOpenTrips = async () => {
 
   return result.rows;
 };
+
+/**
+ * Queries the database asynchronously to get trip data.
+ *
+ * @param id - The ID of the trip to get data for.
+ *
+ * @returns A JSON object representing the trip data if the trip exists, else undefined.
+ */
+export async function getTripData(id: number) {
+  let result: typeof QueryResult = null;
+  const client = await pool.connect();
+
+  try {
+    result = await client.query(
+      `SELECT
+                        trip_id,
+                        name,
+                        startdate AT TIME ZONE 'UTC' AS startdate,
+                        enddate AT TIME ZONE 'UTC' AS enddate,
+                        category,
+                        sport,
+                        location,
+                        difficulty,
+                        description,
+                        signup
+                        
+                    FROM trip WHERE trip_id=$1;`,
+      [id],
+    );
+
+    return result.rows[0];
+  } catch (error: any) {
+    //Intentionally left blank
+  } finally {
+    client.release();
+  }
+}
+

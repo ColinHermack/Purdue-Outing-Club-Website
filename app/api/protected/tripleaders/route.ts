924e5441093ffb7b782284c082e92113e78416fd
Colin Hermack <chermack@purdue.edu>
2026-06-16 22:12:10 -0400

    Updated trip leaders API endpoint with post and push endpoints

--- app/api/protected/tripleaders/route.ts
+++ app/api/protected/tripleaders/route.ts
@@ -1,7 +1,8 @@
 "use server";
 
 /**
- * A protected API route that will allow authorized users to get a list of trip leaders or add a new trip leader. 
+ * A protected API route that will allow authorized users to get a list of trip leaders, add a new trip leader, or
+ * update an existing trip leader.
  *
  * @author Colin Hermack
  */
@@ -11,8 +12,12 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 import OfficerDTO from "@/dtos/officerDto";
 import TripLeaderDTO from "@/dtos/tripLeaderDto";
+import MemberDTO from '@/dtos/memberDto';
+import CreateTripLeaderDTO from "@/dtos/createTripLeaderDto";
+import UpdateTripLeaderDTO from "@/dtos/updateTripLeaderDto";
 import { getOfficerDataByEmail } from "@/miniservices/officerMiniService";
-import { getAllTripLeaders } from "@/miniservices/tripLeaderMiniService";
+import { getAllTripLeaders, createTripLeader, getTripLeader, updateTripLeader } from "@/miniservices/tripLeaderMiniService";
+import { getMemberById } from '@/miniservices/memberMiniService';
 
 import { GET_TRIP_LEADERS_AUTHORIZED_POSITIONS } from "@/config/permissions";
 
@@ -22,27 +27,128 @@ import { GET_TRIP_LEADERS_AUTHORIZED_POSITIONS } from "@/config/permissions";
  * @returns An HTTP response object
  */
 export async function GET(): Promise<Response> {
-    const session = await getServerSession(authOptions);
-    
+    try {
+        const session = await getServerSession(authOptions);
 
-    if (!session || session.user === undefined || typeof session.user.email !== "string") {
-        return new Response("Unauthorized", {status: 401});
+        if (!session || session.user === undefined || typeof session.user.email !== "string") {
+            return new Response("Unauthorized", {status: 403});
+        }
+
+        let userOfficerPositions: OfficerDTO[] | null = await getOfficerDataByEmail(session.user.email);
+
+        if (userOfficerPositions === null) {
+            return new Response("Forbidden", { status: 401 });
+        }
+
+        const userIsAuthorized: boolean = userOfficerPositions.some(
+            (officer: OfficerDTO) => officer.position !== undefined && GET_TRIP_LEADERS_AUTHORIZED_POSITIONS.includes(officer.position)
+        );
+
+        if (!userIsAuthorized) {
+            return new Response("Forbidden", { status: 401 });
+        }
+
+        const tripLeaders: TripLeaderDTO[] = await getAllTripLeaders();
+        return new Response(JSON.stringify(tripLeaders), { status: 200 });
+    } catch (error: any) {
+        return new Response("Internal Server Error", { status: 500 });
     }
+}
 
-    let userOfficerPositions: OfficerDTO[] | null = await getOfficerDataByEmail(session.user.email);
+/**
+ * Creates a new trip leader in the trip leaders table.
+ * 
+ * @returns An HTTP response object
+ */
+export async function POST(newTripLeader: CreateTripLeaderDTO): Promise<Response> {
+    try {
+        const session = await getServerSession(authOptions);
 
-    if (userOfficerPositions === null) {
-        return new Response("Forbidden", { status: 401 });
+        if (!session || session.user === undefined || typeof session.user.email !== "string") {
+            return new Response("Unauthorized", {status: 403});
+        }
+
+        let userOfficerPositions: OfficerDTO[] | null = await getOfficerDataByEmail(session.user.email);
+
+        if (userOfficerPositions === null) {
+            return new Response("Forbidden", { status: 401 });
+        }
+
+        const userIsAuthorized: boolean = userOfficerPositions.some(
+            (officer: OfficerDTO) => officer.position !== undefined && GET_TRIP_LEADERS_AUTHORIZED_POSITIONS.includes(officer.position)
+        );
+
+        if (!userIsAuthorized) {
+            return new Response("Forbidden", { status: 401 });
+        }
+
+        if (newTripLeader.memberId == null) {
+            return new Response("Field memberId is required", { status: 400 });
+        }
+
+        let member: MemberDTO | null = await getMemberById(newTripLeader.memberId);
+        if (member == null) {
+            return new Response(`No member with id ${newTripLeader.memberId} exists.`);
+        }
+
+        let didCreateTripLeader: boolean = await createTripLeader(newTripLeader);
+        if (!didCreateTripLeader) {
+            return new Response("Internal Server Error", { status: 500 });
+        }
+
+        let createdTripLeader: TripLeaderDTO = await getTripLeader(newTripLeader.memberId);
+        return new Response(JSON.stringify(createdTripLeader), { status: 200 });
+
+    } catch (error: any) {
+        return new Response("Internal Server Error", { status: 500 });
     }
+}
 
-    const userIsAuthorized: boolean = userOfficerPositions.some(
-        (officer: OfficerDTO) => officer.position !== undefined && GET_TRIP_LEADERS_AUTHORIZED_POSITIONS.includes(officer.position)
-    );
+/**
+ * Updates an existing trip leader in the trip leaders table.
+ *
+ * @returns An HTTP response object
+ */
+export async function PUT(updatedTripLeader: UpdateTripLeaderDTO): Promise<Response> {
+    try {
+        const session = await getServerSession(authOptions);
 
-    if (!userIsAuthorized) {
-        return new Response("Forbidden", { status: 401 });
+        if (!session || session.user === undefined || typeof session.user.email !== "string") {
+            return new Response("Unauthorized", {status: 403});
+        }
+
+        let userOfficerPositions: OfficerDTO[] | null = await getOfficerDataByEmail(session.user.email);
+
+        if (userOfficerPositions === null) {
+            return new Response("Forbidden", { status: 401 });
+        }
+
+        const userIsAuthorized: boolean = userOfficerPositions.some(
+            (officer: OfficerDTO) => officer.position !== undefined && GET_TRIP_LEADERS_AUTHORIZED_POSITIONS.includes(officer.position)
+        );
+
+        if (!userIsAuthorized) {
+            return new Response("Forbidden", { status: 401 });
+        }
+
+        if (updatedTripLeader.memberId == null) {
+            return new Response("Field memberId is required", { status: 400 });
+        }
+
+        let member: MemberDTO | null = await getMemberById(updatedTripLeader.memberId);
+        if (member == null) {
+            return new Response(`No member with id ${updatedTripLeader.memberId} exists.`);
+        }
+
+        let didUpdateTripLeader: boolean = await updateTripLeader(updatedTripLeader);
+        if (!didUpdateTripLeader) {
+            return new Response("Internal Server Error", { status: 500 });
+        }
+
+        let tripLeader: TripLeaderDTO = await getTripLeader(updatedTripLeader.memberId);
+        return new Response(JSON.stringify(tripLeader), { status: 200 });
+
+    } catch (error: any) {
+        return new Response("Internal Server Error", { status: 500 });
     }
-
-    const tripLeaders: TripLeaderDTO[] = await getAllTripLeaders();
-    return new Response(JSON.stringify(tripLeaders), { status: 200 });
 }
--- miniservices/tripLeaderMiniService.ts
+++ miniservices/tripLeaderMiniService.ts
@@ -7,6 +7,8 @@
  */
 
 import TripLeaderDTO from "@/dtos/tripLeaderDto";
+import CreateTripLeaderDTO from "@/dtos/createTripLeaderDto";
+import UpdateTripLeaderDTO from "@/dtos/updateTripLeaderDto";
 
 const { Pool, QueryResult } = require("pg");
 const pool = new Pool({
@@ -73,7 +75,7 @@ export async function getAllTripLeaders(): Promise<TripLeaderDTO[]> {
               type: row.first_aid_data.Type,
               expires: row.first_aid_data.Expires,
               verified: row.first_aid_data.Verified,
-            } : null,
+            } : undefined,
             carData: row.car_data,
             driverData: row.driver_data,
             emergencyData: row.emergency_data ? {
@@ -81,7 +83,7 @@ export async function getAllTripLeaders(): Promise<TripLeaderDTO[]> {
               email: row.emergency_data.Email,
               phone: row.emergency_data.Phone,
               relation: row.emergency_data.Relation,
-            } : null,
+            } : undefined,
             policyAgreement: row.policy_agreement,
             waiverAgreement: row.waiver_agreement,
             schoolYear: row.school_year,
@@ -89,7 +91,7 @@ export async function getAllTripLeaders(): Promise<TripLeaderDTO[]> {
               allergies: row.medical_data.Allergies,
               conditions: row.medical_data.Conditions,
               medications: row.medical_data.Medications,
-            } : null,
+            } : undefined,
             tripCount: row.trip_count,
             holds: row.holds,
             signupCount: row.signup_count,
@@ -109,3 +111,152 @@ export async function getAllTripLeaders(): Promise<TripLeaderDTO[]> {
     }
 }
 
+/**
+ * Gets a specific trip leader based on their member id
+ * 
+ * @param memberId The member id corresponding to the trip leader
+ */
+export async function getTripLeader(memberId: number): Promise<TripLeaderDTO> {
+  const client = await pool.connect();
+
+  try {
+    let result = await client.query(`
+        SELECT
+          m.member_id,
+          m.name,
+          m.pronouns,
+          m.email,
+          m.phone,
+          m.dues_data,
+          m.first_aid_data,
+          m.car_data,
+          m.driver_data,
+          m.emergency_data,
+          m.policy_agreement,
+          m.waiver_agreement,
+          m.school_year,
+          m.medical_data,
+          m.trip_count,
+          m.holds,
+          m.signup_count,
+          m.years_active,
+          m.campus,
+          t.sport,
+          t.process,
+          t.lead_count,
+          t.gmail
+        FROM trip_leader AS t
+        JOIN member AS m ON t.member_id = m.member_id
+        WHERE m.member_id = $1;
+      `,
+      [memberId]
+    );
+
+    let row = result.rows[0];
+
+    return {
+          member: {
+            id: row.member_id,
+            name: row.name,
+            pronouns: row.pronouns,
+            email: row.email,
+            phone: row.phone,
+            duesData: row.dues_data,
+            firstAidData: row.first_aid_data ? {
+              type: row.first_aid_data.Type,
+              expires: row.first_aid_data.Expires,
+              verified: row.first_aid_data.Verified,
+            } : undefined,
+            carData: row.car_data,
+            driverData: row.driver_data,
+            emergencyData: row.emergency_data ? {
+              name: row.emergency_data.Name,
+              email: row.emergency_data.Email,
+              phone: row.emergency_data.Phone,
+              relation: row.emergency_data.Relation,
+            } : undefined,
+            policyAgreement: row.policy_agreement,
+            waiverAgreement: row.waiver_agreement,
+            schoolYear: row.school_year,
+            medicalData: row.medical_data ? {
+              allergies: row.medical_data.Allergies,
+              conditions: row.medical_data.Conditions,
+              medications: row.medical_data.Medications,
+            } : undefined,
+            tripCount: row.trip_count,
+            holds: row.holds,
+            signupCount: row.signup_count,
+            yearsActive: row.years_active,
+            campus: row.campus,
+          },
+          sport: row.sport,
+          process: row.process,
+          leadCount: row.lead_count,
+          gmail: row.gmail,
+        }
+
+  } catch (error: any) {
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+/**
+ * Updates an existing entry in the trip leader table of the database.
+ *
+ * @param updatedTripLeader Data for the updated trip leader entry
+ */
+export async function updateTripLeader(updatedTripLeader: UpdateTripLeaderDTO): Promise<boolean> {
+  const client = await pool.connect();
+
+  try {
+    await client.query(
+      "UPDATE trip_leader SET sport = '$1', process = '{\"shadow\": $2, \"approved\": $3, \"certified\": $4}', gmail = '$5' WHERE member_id = $6;",
+      [
+        updatedTripLeader.sport?.concat(", "),
+        updatedTripLeader.process?.shadow,
+        updatedTripLeader.process?.approved,
+        updatedTripLeader.process?.certified,
+        updatedTripLeader.gmail,
+        updatedTripLeader.memberId,
+      ]
+    );
+
+    return true;
+
+  } catch (error: any) {
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+/**
+ * Creates a new entry in the trip leader table of the database.
+ *
+ * @param newTripLeader Data for the new trip leader entry
+ */
+export async function createTripLeader(newTripLeader: CreateTripLeaderDTO): Promise<boolean> {
+  const client = await pool.connect();
+
+  try {
+    await client.query(
+      "INSERT INTO trip_leader VALUES ($1, '$2', '{\"shadow\": $3, \"approved\": $4, \"certified\": $5}', 0, '$6');", 
+      [
+        newTripLeader.memberId,
+        newTripLeader.sport?.concat(", "),
+        newTripLeader.process?.shadow,
+        newTripLeader.process?.approved,
+        newTripLeader.process?.certified,
+        newTripLeader.gmail
+      ]);
+
+      return true;
+    
+  } catch (error: any) {
+    throw error;
+  } finally {
+    client.release();
+  }
+}
